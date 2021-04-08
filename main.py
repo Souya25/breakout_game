320226b99a7b0e03d89a65da1847c44a49855de9
Souya <amatou08252525@gmail.com>
2021-04-09 02:10:13 +0900

    add CONSTANT and circle

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pygame
 from pygame.locals import *
 import sys
 
+#定数を定義
+RADIUS = 10
+FRAME_LOWER = 50
+FRAME_UPPER = 550
+FRAME_LEFT = 200
+FRAME_RIGHT = 600
+
+
 def main():
     pygame.init() # 初期化
     screen = pygame.display.set_mode((800, 600)) # ウィンドウサイズの指定
@@ -12,7 +20,10 @@ def main():
         screen.fill((0,0,0,)) # 背景色の指定。RGBだと思う
         
         #白枠を表示
-        pygame.draw.lines( screen, (255, 255, 255), True, ( (200, 50), ( 600, 50), ( 600, 550), ( 200, 550)))
+        pygame.draw.lines( screen, (255, 255, 255), True, ( (FRAME_LEFT, FRAME_UPPER), ( FRAME_RIGHT, FRAME_UPPER), ( FRAME_RIGHT, FRAME_LOWER), ( FRAME_LEFT,FRAME_LOWER)))
+        
+        #ボールを表示
+        pygame.draw.circle(screen, ( 255, 0, 0), (400, 200), RADIUS)
 
         pygame.display.update() # 画面更新
         
